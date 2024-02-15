42087d48ed5557287118d198fd466184317da7c5
Rudransh Shinghal <84223519+Ansh1902396@users.noreply.github.com>
2024-02-16 02:30:52 +0530

    done

--- liftoff/components/Team.jsx
+++ liftoff/components/Team.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Card, Metric, Text  } from "@tremor/react";
 
-export default () => (
+export const Team =  () => (
     <Card className="max-w-xs mx-auto" decoration="top" decorationColor="indigo">
       <Text>Sales</Text>
       <Metric>$ 34,743</Metric>
